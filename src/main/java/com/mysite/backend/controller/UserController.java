b0b10cb79662fe2882fc7ed27e3765c515ffcaba
[JUNG RYUN KIM] <[kim665700@gmail.com]>
2025-01-31 16:05:36 +0900

    userList

--- src/main/java/com/mysite/backend/controller/UserController.java
+++ src/main/java/com/mysite/backend/controller/UserController.java
@@ -2,19 +2,26 @@ package com.mysite.backend.controller;
 import com.mysite.backend.model.User;
 import com.mysite.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 //@ResponseBody 포함(화면 대신 바로 데이터 보냄)
 @RestController
+@CrossOrigin("*")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
-    @PostMapping("/user") //RequestBody 유저 객체를 바로 보내준다.
+    @PostMapping("/users") //RequestBody 유저 객체를(JSON) 바로 보내준다.
     User newUser(@RequestBody User user) {
         return userRepository.save(user);
     }
+
+    @GetMapping("/users")
+    List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
 }
